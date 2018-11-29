dc9658b40f9f4caa8e4876a8e1460b048dd01197
Jayaleshwari <jayaleshwarin@syncfusion.com>
2018-11-29 16:20:16 +0530

    GroupHeaderTemplate format sample committed.

---
+++ GroupHeaderTemplate/SfListViewSample/SfListViewSample/Helpers/GroupHeaderConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SfListViewSample
+{
+    public class GroupHeaderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType.Name == "Color")
+            {
+                if ((int)value == 1)
+                    return Color.Aqua;
+                else
+                    return Color.AntiqueWhite;
+            }
+            else
+            {
+                if ((int)value == 1)
+                    return new Thickness(5, 5, 5, 0);
+                else
+                    return new Thickness((int)value * 15, 5, 5, 0);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ GroupHeaderTemplate/SfListViewSample/SfListViewSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfListViewSample
+{
+	  public partial class MainPage : ContentPage
+    {
+        #region Constructor        
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+        #endregion
+    }
+    
+}
